45991d593d0ca9472049319fb001aab43379fd8b
atal435-0001 <inoueyutaka@deliv.co.jp>
2025-02-13 17:37:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,5 @@ if ingredients_list:
 
 # New section to display smootheifroot nutrition infomation
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
